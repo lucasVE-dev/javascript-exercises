0f3d450ef2be76ed50ffe409950becd463f3dbdd
Lucas Villalba <283061802+lucasVE-dev@users.noreply.github.com>
2026-09-22 20:02:52 +1000

    Exercise completed easily

--- foundations/loops_and_arrays/01_repeatString/repeatString.js
+++ foundations/loops_and_arrays/01_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    if(num<0){
+        return "ERROR";
+    }
+    let newWord = ""
+    for (let i=0; i<num; i++) {
+        newWord = newWord + str
+    }
+    return newWord;
 };
 
 // Do not edit below this line
