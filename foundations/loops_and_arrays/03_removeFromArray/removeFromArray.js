87dbed11961a16317319b4a7968ff941413c1300
Lucas Villalba <283061802+lucasVE-dev@users.noreply.github.com>
2026-09-22 20:21:41 +1000

    Solved using the filter method

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...valuesToBeRemoved) {
+    for (let valueToBeRemoved of valuesToBeRemoved){
+        array = array.filter( value => value !== valueToBeRemoved)
+    }
+    return array;
 };
 
 // Do not edit below this line
